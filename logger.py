fixed wrong scaling; more patience

logger.py
@@ -29,7 +29,7 @@ class MultiInputLogger(template.Logger):
                 both[i]=self.testImages[int(i/2)][1]
             else:
                 both[i]=predictions[int(i/2)]
-        stacked = self.stack(both)
+        stacked = self.stack(both)*255
         cv2.imwrite(name,stacked)
         conv = np.array([np.clip(stacked,0,255)],dtype=np.uint8)
         with fileWriter.as_default():
@@ -43,7 +43,7 @@ class MultiInputLogger(template.Logger):
         
         imgs = []
         for img in imgpaths:
-            imgs.append(cv2.imread(F"{testImageFolder}/{img}"))
+            imgs.append(cv2.imread(F"{testImageFolder}/{img}")/255)
         
         for sequence in range(self.sequenceLength):
             in_ = []

trainState.py
@@ -3,7 +3,7 @@ import datamanager,logger
 import TF2_Keras_Template as template
 import tensorflow as tf
 
-batchsize = 8
+batchsize = 4
 
 
 net = StateNetwork.NeuralNetwork()
@@ -16,8 +16,8 @@ valData = datamanager.ValidationDataState("data/test")
 #Get Loggers
 logger = template.Logger("savedata/state/",model)
 logger.setTestImages("data/test")
-callbacks = logger.getCallbacks(period=1) 
-callbacks.append(tf.keras.callbacks.EarlyStopping(patience=20,verbose=True,mode="min"))
+callbacks = logger.getCallbacks(period=5) 
+callbacks.append(tf.keras.callbacks.EarlyStopping(patience=50,verbose=True,mode="min"))
 
 
 model.fit(dataGen.getGenerator(),