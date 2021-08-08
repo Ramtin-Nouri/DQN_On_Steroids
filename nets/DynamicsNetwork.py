0cdb30bd7edcfd517282bda64bfd52bc40799cee
17nouri <7nouri@informatikum.uni-hamburg.de>
2021-08-08 13:35:02 +0200

    Added custom loss to empasize ball

--- nets/DynamicsNetwork.py
+++ nets/DynamicsNetwork.py
@@ -1,9 +1,11 @@
 from tensorflow.keras.layers import Conv2D, Dropout, MaxPooling2D, UpSampling2D, concatenate, Input, Dense
 from tensorflow.keras.models import Model
+from tensorflow.python.keras.layers.normalization_v2 import BatchNormalization
 from tensorflow_addons.optimizers import AdamW
 
 import TF2_Keras_Template as template
 import tensorflow as tf
+import numpy as np
 tf.random.set_seed(111)
 
 
@@ -45,6 +47,14 @@ class NeuralNetwork(template.nnBase.NNBase):
         x = Dropout(0.1)(x)
         out = Conv2D(3, (3, 3), activation='relu',padding='same')(x)
               
+        #Weighting red value more, so that the ball doesn't disappear
+        def weightedMSE(y_true, y_pred):
+            red = np.array(np.all(y_true.numpy()==(200/255,72/255,72/255),axis=-1),dtype=np.float32)*3+1
+            mse = tf.math.reduce_mean(tf.math.pow(y_pred-y_true,4))
+            
+            return tf.multiply(mse, red)
+
         model = Model([observations_input,action_input],out)
-        model.compile(optimizer=AdamW(weight_decay=1e-4), loss='mse')
+        model.compile(optimizer=AdamW(weight_decay=1e-4), loss=weightedMSE,run_eagerly=True)
         return model
+
