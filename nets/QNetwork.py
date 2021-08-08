ff5b69207113917c02397bf4be0a1622d58b2fd7
17nouri <7nouri@informatikum.uni-hamburg.de>
2021-08-08 17:50:41 +0200

    new DQN architecture

--- nets/QNetwork.py
+++ nets/QNetwork.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Conv2D, Dropout, MaxPooling2D, UpSampling2D, concatenate, Input, Dense
+from tensorflow.keras.layers import Conv2D, Flatten, MaxPooling2D, UpSampling2D, concatenate, Input, Dense
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 
@@ -17,7 +17,7 @@ class NeuralNetwork(template.nnBase.NNBase):
             Instead of outputShape as intended ,use second parameter for list of arguments
         """
         [outputShape,basemodel,learningRate,lrdecay] = args
-        layer_name = 'dense'
+        layer_name = 'encoding'
         encoder = Model(inputs=basemodel.layers[0].input,
                                        outputs=basemodel.get_layer(layer_name).output)
         
@@ -25,12 +25,13 @@ class NeuralNetwork(template.nnBase.NNBase):
             
         input_ = Input(inputShape)
         x = encoder(input_)
-        dense1 = Dense(200)(x)
-        dense2 = Dense(100)(dense1)
-        dense3 = Dense(50)(dense2)
-        dense4 = Dense(outputShape)(dense3)
+        x = Conv2D(8,(3,3),strides=(2,2))(x)
+        x = Conv2D(8,(3,3),strides=(2,2))(x)
+        x = Conv2D(8,(3,3),strides=(2,2))(x)
+        x = Flatten()(x)
+        x = Dense(outputShape)(x)
 
-        model = Model(inputs=input_,outputs=dense4)
+        model = Model(inputs=input_,outputs=x)
         
         model.compile(loss='mse',optimizer=Adam(lr=learningRate, decay = lrdecay))
         return model
