d179396ffceb500deba6708f14a4e2a9491a381f
17nouri <7nouri@informatikum.uni-hamburg.de>
2021-08-20 22:45:58 +0200

    complete restructuring of DQN trainer; added Double DQN and multiprocessing

--- trainAgent.py
+++ trainAgent.py
@@ -1,110 +1,167 @@
+from threading import Thread
 from nets import QNetwork
 import TF2_Keras_Template as template
-import gym,random,numpy as np, sys
+import gym,random,numpy as np, sys,time,datetime,os
 from collections import deque
 from tqdm import tqdm
 import tensorflow as tf
 from tensorflow.keras.models import load_model,Model
 random.seed(459)
 
-maxQueueSize=10000
+maxQueueSize=1000
 totalSteps = 2000
 totalEpisodes = 10000
 learningRate = 0.01
 lrdecay = 0.01
 gamma = 0.95
-batchSize = 2048
+batchSize = 4
 
 epsilon = 1.0
 min_epsilon = 0.01
 max_epsilon = 1.0
 decay = 0.01
 
-env=gym.make("Breakout-v4")
-env.seed(459)
+class Environment():
+    """
+        Custom Wrapper around OpenAi Gym Environment running observations through an encoder model first
+        And other useful functions
+    """
+    def __init__(self):
+        self.gym_env=gym.make("Breakout-v4")
+        self.gym_env.seed(459)
 
-#Get Encoder Model
-try:
-    autoencoderModel = load_model(sys.argv[1],compile=False)
-except:
-    print("ERROR: Please specify Path to encoder model as first parameter")
-    quit()
-layer_name = 'encoding'
-encoder = Model(inputs=autoencoderModel.layers[0].input,
-                                outputs=autoencoderModel.get_layer(layer_name).output)
+        #Get Autoencoder Model:
+        try:
+            autoencoder = load_model(sys.argv[1],compile=False)
+        except:
+            print("ERROR: Please specify Path to encoder model as first parameter")
+            quit()
+        #Extract Encoder part from autoencoder:
+        layer_name = 'encoding'
+        self.model = Model(inputs=autoencoder.layers[0].input,outputs=autoencoder.get_layer(layer_name).output)
+
+    def getOutputShape(self):
+        return self.model.output_shape[1:]
+    
+    def getActionSpace(self):
+        return self.gym_env.action_space.n
+
+    def getSample(self):
+        return self.gym_env.action_space.sample()
+
+    def reset(self):
+        obs = self.gym_env.reset()
+        obs = np.array([obs[2:]])
+        return self.model.predict(obs)[0]
+
+
+    def step(self,action):
+        obs, reward, done, _ = self.gym_env.step(action)
+        obs = np.array([obs[2:]])
+        return self.model.predict(obs)[0], reward, done
 
             
+class Agent(Thread):
+    """
+        Agent sampling actions and filling the memory.
+        It uses a copy of the model that is only updated once in a while
+    """
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.env = Environment()
+        self.model = net.getModel(self.env.getOutputShape(),[self.env.getActionSpace(),learningRate,lrdecay])
+        self.memory = deque(maxlen=maxQueueSize)
+
+    def getAction(self,state):
+        exploit= random.uniform(0,1)
+        if exploit > epsilon:
+            prediction = self.model.predict(np.array([state]))[0]
+            return prediction.argmax()
+        else:
+            return self.env.getSample()
+
+    def getMemorySample(self):
+        return np.array(random.sample(self.memory,batchSize))
+
+    def runEpisode(self):
+        score = 0
+        state = self.env.reset()
+        for _ in tqdm(range(totalSteps)):
+            action = self.getAction(state)
+            newState, reward, done = self.env.step(action)
+            
+            score += reward
+                
+            self.memory.append((state,action,reward,newState,done))
+            if done:
+                break            
+            state = newState
+        return score
+    
+    def run(self):
+        highScore = 0
+        scoreQue = deque(maxlen=100)
+        for episode in range(totalEpisodes):
+            score = self.runEpisode()
+            if score > highScore:
+                highScore = score
+            epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay*episode)
+            scoreQue.append(score)
+            print("Episode: ",episode, "| Score: " ,score,"| HighScore: ",highScore, "| Epsilon: ", epsilon, "Running Mean: ", np.mean(scoreQue))
+            tf.summary.scalar("Score",score,step=episode)
+            tf.summary.scalar("Highscore",highScore,step=episode)
+            tf.summary.scalar("Epsilon",epsilon,step=episode)
+            tf.summary.scalar("Running Mean",np.mean(scoreQue),step=episode)
+
+class Trainer(Thread):
+    """
+        Samples from Agents memory and trains network
+    """
+    def __init__(self,agent):
+        Thread.__init__(self)
+        self.model,self.epoch = net.getModel(agent.env.getOutputShape(),[agent.env.getActionSpace(),learningRate,lrdecay])
+        self.agent = agent
+
+    def run(self):
+        while True:
+            minibatch = self.agent.getMemorySample()
+            targets = []
+            w,h,c = agent.env.getOutputShape()
+            states = np.concatenate(minibatch[:,0]).reshape(batchSize,w,h,c)
+            newStates = np.concatenate(minibatch[:,3]).reshape(batchSize,w,h,c)
+            qValues = self.model.predict(newStates,batch_size=batchSize )
+            allTargets = self.model.predict(states,batch_size=batchSize )
+            for i, (_, action, reward, _, done) in enumerate(minibatch):
+                if done:
+                    target = reward
+                else:
+                    target = reward + gamma * np.amax(qValues[i])
+                allTargets[i][action]=target
+                targets.append(allTargets[i])
+
+            loss = self.model.train_on_batch(np.array(states),np.array(targets))
+            tf.summary.scalar("Loss",loss,step=self.epoch)
+            #print(F"Epoch {self.epoch} ; Loss: {loss}")
+            self.epoch +=1
 
 
-net = QNetwork.NeuralNetwork()
-model,epoch = net.getModel(encoder.output_shape[1:],[env.action_space.n,learningRate,lrdecay])
+#Get Tensorboard filewriter
+today = datetime.datetime.today()
+folderName = "%s%04d-%02d-%02d-%02d-%02d-%02d/" % ("savedata/agent/",today.year,today.month,today.day,today.hour,today.minute,today.second)
+os.makedirs("%s"%(folderName))
 
-#Get Loggers
-logger = template.Logger("savedata/agent/",model)
-file_writer = tf.summary.create_file_writer(logger.folderName)
+file_writer = tf.summary.create_file_writer(folderName)
 file_writer.set_as_default()
 
 
-def getAction(state):
-    exploit= random.uniform(0,1)
-    if exploit > epsilon:
-        prediction = model.predict(np.array([state]))[0]
-        return prediction.argmax()
-    else:
-        return env.action_space.sample()
+net = QNetwork.NeuralNetwork()
+agent = Agent()
+trainer = Trainer(agent)
 
-def replay():
-    minibatch = random.sample(memory,batchSize)
-    states = []
-    targets = []
-    for state, action, reward, nextState, done in minibatch:
-        states.append(state)
-        if done:
-            target = reward
-        else:
-            target = reward + gamma*np.amax(model.predict(np.array([nextState])))
-        target_f = model.predict(np.array([state]))[0]
-        target_f[action]=target
-        targets.append(target_f)
-    model.train_on_batch(np.array(states),np.array(targets))
+with open('%s/architecture.txt'%(folderName),'w') as fh:
+    trainer.model.summary(print_fn=lambda x: fh.write(x + '\n'))
 
-
-def runEpisode():
-    score = 0
-    obs = env.reset()[2:]
-    state = encoder.predict(np.array([obs]))[0]
-    for _ in tqdm(range(totalSteps)):
-        action = getAction(state)
-        obs, reward, done, _ = env.step(action)
-        obs = np.array([obs[2:]])
-        newState = encoder.predict(obs)[0]
-        
-        score += reward
-            
-        memory.append((state,action,reward,newState,done))
-        if done:
-            break
-        if len(memory)>=batchSize:
-            replay()
-        
-        state = newState
-    return score
-    
-   
-    
-memory = deque(maxlen=maxQueueSize)
-highScore = 0
-scoreQue = deque(maxlen=100)
-
-for episode in range(totalEpisodes):
-    score = runEpisode()
-    if score > highScore:
-        highScore = score
-    epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay*episode)
-    scoreQue.append(score)
-    rm = np.mean(scoreQue)
-    print("Episode: ",episode, "| Score: " ,score,"| HighScore: ",highScore, "| Epsilon: ", epsilon, "Running Mean: ", np.mean(scoreQue))
-    tf.summary.scalar("Score",score,step=episode)
-    tf.summary.scalar("Highscore",highScore,step=episode)
-    tf.summary.scalar("Epsilon",epsilon,step=episode)
-    tf.summary.scalar("Running Mean",np.mean(scoreQue),step=episode)
+agent.start()
+while len(agent.memory) < batchSize:time.sleep(1)
+trainer.start()
